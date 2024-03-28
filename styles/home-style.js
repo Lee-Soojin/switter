5fbdc395294f49e9271e2c003d58edb758553fb5
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-03-28 16:29:04 +0900

    design: Create HomePageLayout

---
+++ styles/home-style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HomePageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 70px 0;
+  width: 100vw;
+  min-width: 100vw;
+  height: 100vh;
+  min-height: 100vh;
+  background-color: #f6f6f6;
+`;
