53558da9053aa9bc1fd3360890fc9a4c64537b40
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-03-28 14:29:18 +0900

    feat: Wrap the children of the layout with the style registry component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,11 @@
+import StyledComponentsRegistry from "@/styles/registry";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
