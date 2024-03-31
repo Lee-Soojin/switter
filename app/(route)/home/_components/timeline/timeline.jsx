8729b65e5c35bdfa3980fc9e2955bd447371012a
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-03-31 17:20:59 +0900

    feat: Apply GET API & Add tweets state

--- app/(route)/home/_components/timeline/timeline.jsx
+++ app/(route)/home/_components/timeline/timeline.jsx
@@ -3,8 +3,28 @@
 import { useEffect, useState } from "react";
 import TweetItem from "./tweet_item";
 import { TimelineBox } from "@/styles/timeline-style";
+const apiURL = "http://localhost:8080";
 
 const Timeline = () => {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    const getTweets = () => {
+      fetch(apiURL + "/tweets", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setTweets(data.tweetList);
+        })
+        .catch(console.error);
+    };
+    getTweets();
+  }, [setTweets]);
+
   return (
     <TimelineBox>
       {tweets && tweets.map((x) => <TweetItem key={x.id} data={x} />)}
