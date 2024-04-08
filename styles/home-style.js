c861ffdc7b6b1fd326847ecfebae627fcb7b4ae6
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-04-08 23:15:53 +0900

    design: style of panels

--- styles/home-style.js
+++ styles/home-style.js
@@ -2,18 +2,27 @@ import styled from "styled-components";
 
 export const HomePageLayout = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  gap: 32px;
   padding: 70px 0;
   width: 100vw;
   min-width: 100vw;
   height: 100vh;
   min-height: 100vh;
   background-color: #f6f6f6;
-  > div {
+  > div.home__mainPanel {
     display: flex;
     flex-direction: column;
     width: fit-content;
+    height: 100%;
+    gap: 20px;
+  }
+  > div.home__menuPanel,
+  > div.home__followerPanel {
+    display: flex;
+    flex-direction: column;
+    width: 300px;
     height: 100%;
     gap: 20px;
   }
