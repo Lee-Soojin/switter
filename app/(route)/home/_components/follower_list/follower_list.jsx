42062ae7b986c7909a751307a6b8219915d4b4e9
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-05-01 10:16:36 +0900

    fix: Warning: React does not recognize the prop on DOM

--- app/(route)/home/_components/follower_list/follower_list.jsx
+++ app/(route)/home/_components/follower_list/follower_list.jsx
@@ -29,18 +29,20 @@ const FollowerList = () => {
 
 const FollowerListItem = ({ data }) => {
   return (
-    <FollowerListItemBox isInFavorites={data.isInFavorites}>
-      <div>
-        <Image
-          src="https://pbs.twimg.com/media/GHLH6fla0AAL0Es?format=jpg&name=900x900"
-          width={40}
-          height={40}
-          alt="profile"
-        />
-        <p>Yugyeom</p>
-      </div>
-      <button>즐겨찾기 ♥︎</button>
-    </FollowerListItemBox>
+    data && (
+      <FollowerListItemBox $isInFavorites={data && data.isInFavorites}>
+        <div>
+          <Image
+            src="https://pbs.twimg.com/media/GHLH6fla0AAL0Es?format=jpg&name=900x900"
+            width={40}
+            height={40}
+            alt="profile"
+          />
+          <p>Yugyeom</p>
+        </div>
+        <button>즐겨찾기 ♥︎</button>
+      </FollowerListItemBox>
+    )
   );
 };
 
