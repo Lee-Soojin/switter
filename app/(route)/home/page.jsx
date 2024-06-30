1c0398f3fc0ddb685aa418aacaa7b28b54c5e87a
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-06-30 14:45:26 +0900

    feat: Add title

--- app/(route)/home/page.jsx
+++ app/(route)/home/page.jsx
@@ -3,10 +3,14 @@ import TweetForm from "./_components/form/form";
 import { HomePageLayout } from "@/styles/home-style";
 import Timeline from "./_components/timeline/timeline";
 import FollowerList from "./_components/follower_list/follower_list";
+import Head from "next/head";
 
 const HomePage = () => {
   return (
     <HomePageLayout>
+      <Head>
+        <title>Switter</title>
+      </Head>
       <div className="home__menuPanel"></div>
       <div className="home__mainPanel">
         <TweetForm />
