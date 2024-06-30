2624b7e613891d9e2aa1504862ba73caf10cf08e
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-06-30 14:44:25 +0900

    design: Add LoginPageLayout

---
+++ styles/login-style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const LoginPageLayout = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 32px;
+  padding: 100px 0 70px 0;
+  width: 100vw;
+  min-width: 100vw;
+  height: 100vh;
+  min-height: 100vh;
+  background-color: #f6f6f6;
+`;
