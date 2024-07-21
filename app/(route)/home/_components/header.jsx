dd8e526c7d1c70a2786f2a3f9fe061e4ea4838a3
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-07-21 11:59:44 +0900

    feat: Add logout button

--- app/(route)/home/_components/header.jsx
+++ app/(route)/home/_components/header.jsx
@@ -1,11 +1,18 @@
-import { HeaderBox } from "@/styles/header";
 import Link from "next/link";
+import { HeaderBox } from "@/styles/header";
+import { useAuth } from "@/app/context/auth_context";
 
 const Header = () => {
+  const { user, logout } = useAuth();
+
   return (
     <HeaderBox>
       <Link href="/">Switter</Link>
-      <Link href="/login">로그인</Link>
+      {user ? (
+        <button onClick={logout}>로그아웃</button>
+      ) : (
+        <Link href="/login">로그인</Link>
+      )}
     </HeaderBox>
   );
 };
