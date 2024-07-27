1c624c945af60989a9bd14bba85fd28e94457966
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-07-27 17:40:43 +0900

    feat: Modify getTweetList function

--- app/(route)/home/_components/timeline/timeline.jsx
+++ app/(route)/home/_components/timeline/timeline.jsx
@@ -1,40 +1,28 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import TweetItem from "./tweet_item";
-import { TimelineBox } from "@/styles/timeline-style";
-import { useAuth } from "@/app/context/auth_context";
-import { socketIO } from "@/app/layout";
 import { useTweetService } from "@/app/context/tweet_context";
-const apiURL = "http://localhost:8080";
+import { TimelineBox } from "@/styles/timeline-style";
 
 const Timeline = (props) => {
-  const { user } = useAuth();
   const [tweets, setTweets] = useState([]);
   const tweetService = useTweetService();
 
-  const getTweets = useCallback(() => {
-    tweetService
-      .getAllTweets()
-      .then((tweets) => setTweets(tweets))
-      .catch(console.error);
-
-    const stopSync = tweetService.onSync((tweet) => onCreated(tweet));
-    return () => stopSync();
-  }, [user, setTweets]);
-
-  const onCreated = (tweet) => {
-    setTweets((tweets) => setTweets([...tweets, tweet]));
-  };
-
   useEffect(() => {
-    getTweets();
-  }, [getTweets]);
+    const getTweetList = () => {
+      tweetService
+        .getAllTweets()
+        .then((tweets) => {
+          setTweets(tweets);
+        })
+        .catch(console.error);
+    };
+    getTweetList();
 
-  // socketIO.on("update", (message) => {
-  //   getTweets();
-  //   console.log(message);
-  // });
+    let stopSync = tweetService.onSync(() => getTweetList());
+    return () => stopSync();
+  }, [tweetService]);
 
   return (
     <TimelineBox>
