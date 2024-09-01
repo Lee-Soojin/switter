b909b93ec3388c256057dcbf46fba7383f8e6cb3
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-09-01 22:48:06 +0900

    feat: Replace codes with function postTweet

--- app/(route)/home/_components/form/form.jsx
+++ app/(route)/home/_components/form/form.jsx
@@ -2,35 +2,26 @@
 
 import { useRef } from "react";
 import Image from "next/image";
-import { TweetFormBox } from "@/styles/form-style";
 import { useAuth } from "@/app/context/auth_context";
+import { useTweetService } from "@/app/context/tweet_context";
+import { TweetFormBox } from "@/styles/form-style";
 const apiURL = "http://localhost:8080";
 
 const TweetForm = () => {
   const { user } = useAuth();
   const inputRef = useRef(null);
+  const tweetService = useTweetService();
 
   const uploadTweet = async (e) => {
     e.preventDefault();
 
-    if (inputRef && inputRef.current && inputRef.current.value) {
-      try {
-        const response = await fetch(apiURL + "/tweets", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${user.token}`,
-          },
-          body: JSON.stringify({
-            tweet: inputRef.current.value,
-          }),
-        });
-
-        const data = await response.json();
-        if (data) inputRef.current.value = null;
-      } catch (error) {
-        console.error(error);
-      }
+    try {
+      let text = inputRef?.current?.value;
+      const response = await tweetService.postTweet(text);
+      const data = await response.json();
+      if (data) inputRef.current.value = null;
+    } catch (err) {
+      console.error(err);
     }
   };
 
@@ -39,7 +30,7 @@ const TweetForm = () => {
       <div>
         <Image
           src={
-            user.image ||
+            user.url ||
             "https://images.unsplash.com/photo-1638643391904-9b551ba91eaa?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           }
           width={40}
