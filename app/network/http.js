5266ff9da70019acf73c69047af3fb838af3bd1e
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-09-05 00:09:59 +0900

    chore: Change error message

--- app/network/http.js
+++ app/network/http.js
@@ -21,7 +21,7 @@ export default class HttpClient {
 
     if (res.status > 299 || res.status < 200) {
       const message =
-        data && data.message ? data.message : "Something went wrong! 🤪";
+        data && data.message ? data.message : "Something went wrong! 🚫";
       const error = new Error(message);
       if (res.status === 401) {
         this.authErrorEventBus.notify(error);
