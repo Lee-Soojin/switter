e3bc6fb0313af63bf4a88c968b47a5f98396495c
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-09-10 00:28:56 +0900

    design: home page title

--- styles/page-style.js
+++ styles/page-style.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const StartPageLayout = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100vw;
@@ -12,29 +11,31 @@ export const StartPageLayout = styled.div`
   background-size: cover;
   > h1 {
     margin: 0;
-    font-size: 80px;
+    color: #ffffff;
+    font-size: 3.2rem;
     line-height: 150%;
     white-space: pre;
-    text-align: center;
+    text-align: left;
     font-weight: 700;
+    text-shadow: 2px 2px 4px gray;
   }
   > a {
     display: flex;
     align-items: center;
     justify-content: center;
-    padding: 20px 40px;
-    font-size: 48px;
-    font-weight: 700;
-    line-height: 100%;
-    color: #fff;
-    text-decoration: none;
+    padding: 20px;
     background: rgba(255, 255, 255, 0.2);
-    border-radius: 16px;
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
     backdrop-filter: blur(5px);
     -webkit-backdrop-filter: blur(5px);
-    border: 2px solid rgba(255, 255, 255, 0.3);
     transition: all ease 0.2s;
+    border: 2px solid rgba(255, 255, 255, 0.3);
+    border-radius: 16px;
+    text-decoration: none;
+    color: #ffffff;
+    font-size: 2.8rem;
+    font-weight: 700;
+    line-height: 100%;
     &:hover {
       transform: scale(1.1);
     }
