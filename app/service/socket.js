7be990fb01d271b8496d324f6bfeb227947ed7ff
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-09-15 17:39:58 +0900

    fix: console.log error message

--- app/service/socket.js
+++ app/service/socket.js
@@ -6,8 +6,8 @@ export default class Socket {
       auth: (cb) => cb({ token: getAccessToken() }),
     });
 
-    this.io.on("connect_error", (error) => {
-      console.error("socket error", error);
+    this.io.on("connect_error", (err) => {
+      console.log("socket error", err.message);
     });
   }
 
