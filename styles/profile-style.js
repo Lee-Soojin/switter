daea54912ba04ecc75556863d7a4f17257b9d955
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-09-15 17:14:52 +0900

    design: Create ProfilePageLayout

---
+++ styles/profile-style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ProfilePageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding-top: var(--header-height);
+  background-color: #f6f6f6;
+  gap: 20px;
+`;
+
