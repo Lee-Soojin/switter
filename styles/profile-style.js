dbe8eb27b78403e80d44619b4143fbd663a8767e
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-09-15 17:15:00 +0900

    design: Create ProfileBox

--- styles/profile-style.js
+++ styles/profile-style.js
@@ -11,3 +11,76 @@ export const ProfilePageLayout = styled.div`
   gap: 20px;
 `;
 
+export const ProfileBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 600px;
+  > div:nth-child(1) {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 60px;
+    min-height: 60px;
+    padding: 0 20px;
+    gap: 12px;
+    background-color: white;
+    border: 1px solid;
+    border-color: rgb(239, 243, 244);
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 4px;
+      > p:nth-child(1) {
+        color: var(--text-black-color);
+        font-size: 2rem;
+        font-weight: 700;
+      }
+      > p {
+        margin: 0;
+        color: var(--text-gray-color);
+        font-size: 1.3rem;
+        font-weight: 500;
+      }
+    }
+  }
+
+  > div:nth-child(2) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: fit-content;
+    padding: 20px 0;
+    gap: 12px;
+    background-color: white;
+    border-bottom: 1px solid rgb(239, 243, 244);
+
+    > img {
+      border-radius: 50%;
+      border: 4px solid var(--main-color);
+    }
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 4px;
+      > p {
+        margin: 0;
+      }
+      > p:nth-child(1) {
+        color: var(--text-black-color);
+        font-size: 2rem;
+        font-weight: 700;
+      }
+      > p:nth-child(2) {
+        color: var(--text-gray-color);
+        font-size: 1.5rem;
+        font-weight: 500;
+      }
+    }
+  }
+`;
